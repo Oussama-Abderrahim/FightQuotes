00d34f2061c0aeab6fb49496e9f0d7fd4b4cb9eb
Oussama LightRay <SakasakyRay@Gmail.com>
2017-08-30 00:03:15 +0100

    listen to env port when deployed

--- server.js
+++ server.js
@@ -35,4 +35,6 @@ app.post("/addQuote", (req, res) => {
     })
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
